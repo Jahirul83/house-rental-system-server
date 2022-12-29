fa35e8851f32282145ab11177e0c5ff4c36c23a9
mainuddin-hasan <mainuddinctg50419@gmail.com>
2022-12-29 20:53:51 +0600

    view show

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -42,6 +42,15 @@ async function run() {
 
             const filter = {};
             const result = await servicesCollection.find(filter).toArray();
+            res.send(result);
+
+        });
+
+        app.get('/services/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await servicesCollection.findOne(filter);
             res.send(result);
 
         });
